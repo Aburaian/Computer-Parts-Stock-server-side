f007ec12d7ff80c9cfa7112c34909631f63e0adf
raiansakil <aburaian182@gmail.com>
2022-05-28 15:04:36 +0600

    added file

---
+++ index.js
@@ -0,0 +1,87 @@
+const express = require("express");
+const app = express();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_PRODUCTS}:${process.env.DB_PASS}@cluster0.f8ddjlb.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const productCollection = client.db("productUser").collection("products");
+    const reviewCollection = client.db("reviewUser").collection("reviews");
+
+    app.get("/products", async (req, res) => {
+      const query = {};
+      const cursor = productCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/product/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await productCollection.findOne(query);
+      res.send(result);
+    });
+
+    // app.put("/product/:id", async (req, res) => {
+    //   const id = req.params.id;
+    //   const newQuantity = req.body;
+    //   const filter = { _id: ObjectId(id) };
+    //   const options = { upsert: true };
+    //   const updateDoc = {
+    //     $set: {
+    //       quantity: newQuantity.quantity,
+    //     },
+    //   };
+    //   const result = await productCollection.updateOne(
+    //     updateDoc,
+    //     filter,
+    //     options
+    //   );
+    //   res.send(result);
+    // });
+
+    // app.post("/product", async (req, res) => {
+    //   const newProduct = req.body;
+    //   const result = await productCollection.insertOne(newProduct);
+    //   res.send(result);
+    // });
+
+    app.delete("/product/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await productCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    app.get("/reviews", async (req, res) => {
+      const query = {};
+      const cursor = reviewCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+  } finally {
+  }
+}
+
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log("listening to port", port);
+});
